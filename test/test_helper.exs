2c5b639a0188cdb656f4a8cfe156cbc3a5593a0d
Roman Heinrich <roman.heinrich@gmail.com>
2016-02-29 16:21:31 +0100

    test_helper with some testing macros

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,10 @@
 ExUnit.start()
+
+defmodule TestHelper do
+  defmacro assert_route_info(swagger_docs, path, expected) do
+    quote do
+      real_info = unquote(swagger_docs) |> get_in([:paths, unquote(path)])
+      assert real_info == unquote(expected)
+    end
+  end
+end
