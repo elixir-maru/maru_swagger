c2618c65d29eddfc70952152f1e3149a12c1b107
Roman Heinrich <roman.heinrich@gmail.com>
2016-03-01 00:38:18 +0100

    [refactoring] splitting into several smaller functions
    problem: 
    - the large extract_params function for non-get requests is very hard to follow for unfamiliar developers
    
    solution: 
    - slit up into shorter functions, give them descriptive names
    - also rename some variables to longer, but more understandable names

--- lib/maru_swagger/params_extractor.ex
+++ lib/maru_swagger/params_extractor.ex
@@ -4,8 +4,8 @@ defmodule MaruSwagger.ParamsExtractor do
     %{ep | method: "MATCH"} |> extract_params
   end
 
-  def extract_params(%Endpoint{method: "GET", path: path, param_context: params_list}) do
-    for param <- params_list do
+  def extract_params(%Endpoint{method: "GET", path: path, param_context: param_context}) do
+    for param <- param_context do
       %{ name:        param.attr_name,
          description: param.desc || "",
          required:    param.required,
@@ -15,47 +15,43 @@ defmodule MaruSwagger.ParamsExtractor do
     end
   end
   def extract_params(%Endpoint{method: "GET"}), do: []
-
   def extract_params(%Endpoint{param_context: []}), do: []
 
-  def extract_params(%Endpoint{path: path, param_context: params_list}) do
-    {file_list, param_list_extra} = split_file_list(params_list) #FIXME find a better name
-    p = convert_maru_params_to_swagger_params(param_list_extra)  #FIXME find a better name
+  def extract_params(%Endpoint{param_context: param_context, path: path}) do
+    {file_param_list, param_list} = split_file_list_and_rest(param_context)
+    file_param_list_swagger       = convert_file_param_list_to_swagger(file_param_list)
+    param_list_swagger            = convert_param_list_to_swagger(param_list)
+    generate_swagger_non_get_data(file_param_list_swagger, param_list_swagger, path)
+  end
 
-    f = for param <- file_list do
-      %{ name:        param.attr_name,
-         in:          "formData",
-         description: "file",
-         required:    true,
-         type:        "file"
-       }
-    end
-
+  def generate_swagger_non_get_data(file_param_list_swagger, param_list_swagger, path) do
     %{ name: "body",
        in: "body",
        description: "desc",
        required: false,
      }
     |> fn r ->
-      if p == %{} do
+      if param_list_swagger == %{} do
         r
       else
         r
         |> put_in([:schema], %{})
-        |> put_in([:schema, :properties], p)
+        |> put_in([:schema, :properties], param_list_swagger)
       end
     end.()
     |> fn r ->
-      if f == [] do [r] else f end
+      if file_param_list_swagger == [] do [r] else file_param_list_swagger end
     end.()
     |> fn r ->
       if Enum.any?(path, &is_atom/1) do
         r ++ (path |> Enum.filter(&is_atom/1) |> Enum.map(&(%{name: &1, in: "path", required: true, type: "string"})))
-      else r end
+      else
+        r
+      end
     end.()
   end
 
-  defp convert_maru_params_to_swagger_params(param_list_extra) do
+  defp convert_param_list_to_swagger(param_list_extra) do
     for %Maru.Router.Param{attr_name: attr_name, parser: parser, required: required, desc: desc} <- param_list_extra do
       { attr_name, %{
           type: decode_parser(parser),
@@ -63,14 +59,25 @@ defmodule MaruSwagger.ParamsExtractor do
           description: desc || "",
         }
       }
-    end |> Enum.into(%{}) #|> IO.inspect
+    end |> Enum.into(%{})
+  end
+
+  defp convert_file_param_list_to_swagger(file_list) do
+     for param <- file_list do
+      %{ name:        param.attr_name,
+         in:          "formData",
+         description: "file",
+         required:    true,
+         type:        "file"
+       }
+    end
   end
 
   defp decode_parser(parser) do
     parser |> to_string |> String.split(".") |> List.last |> String.downcase
   end
 
-  defp split_file_list(params_list) do
+  defp split_file_list_and_rest(params_list) do
      Enum.split_while(params_list, fn(param) -> decode_parser(param.parser) == "file" end)
   end
 end
