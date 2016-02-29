83afa84f44962381c10d94252566afc64aec0bb7
Roman Heinrich <roman.heinrich@gmail.com>
2016-02-29 16:10:34 +0100

    first dummy test

--- test/maru_swagger_test.exs
+++ test/maru_swagger_test.exs
@@ -1,7 +1,39 @@
 defmodule MaruSwaggerTest do
-  use ExUnit.Case
+  use ExSpec, async: true
+  doctest MaruSwagger
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  describe "basic test" do
+    defmodule BasicTest.Homepage do
+      use Maru.Router
+
+      desc "hello world action"
+      params do
+        requires :id, type: Integer
+      end
+      get do
+        %{ hello: :world }
+      end
+    end
+
+    defmodule BasicTest.Api do
+      use Maru.Router
+      mount MaruSwaggerTest.BasicTest.Homepage
+      rescue_from :all do
+        conn
+        |> put_status(500)
+        |> text("Server Error")
+      end
+    end
+
+
+    it "works" do
+      res = MaruSwagger.generate(MaruSwaggerTest.BasicTest.Api, nil, ["/"])
+      expected = %{info: %{title: "Swagger API for MaruSwaggerTest.BasicTest.Api", version: nil},
+  paths: %{"" => %{"get" => %{description: "hello world action", parameters: [],
+        responses: %{"200" => %{description: "ok"}}}}}, swagger: "2.0"}
+
+      assert res == expected
+    end
   end
 end
