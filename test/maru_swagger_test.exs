da0b03420ef7805e2c1e0d7b9e92a4f46f726729
Roman Heinrich <roman.heinrich@gmail.com>
2016-03-02 12:15:41 +0100

    unit tests for init options in the MaruSwagger plug

--- test/maru_swagger_test.exs
+++ test/maru_swagger_test.exs
@@ -33,4 +33,52 @@ defmodule MaruSwaggerTest do
         )
     end
   end
+
+
+
+  describe "MaruSwagger - Plug: init options" do
+    def init(opts) do
+      opts |> MaruSwagger.init
+    end
+
+    it "requires :at for mounting point" do
+      assert {["swagger", "v1"], MaruSwaggerTest.BasicTest.Api, nil, false, [] } == init(
+        at: "swagger/v1",
+        for: BasicTest.Api
+      )
+    end
+
+    it "raises without :at" do
+      assert_raise KeyError, "key :at not found in: [for: MaruSwaggerTest.BasicTest.Api]", fn ->
+        init(for: BasicTest.Api)
+      end
+    end
+
+    it "accepts :version for specified version" do
+      assert {["swagger", "v1"], MaruSwaggerTest.BasicTest.Api, "v1", false, [] } == init(
+        at: "swagger/v1",
+        version: "v1",
+        for: BasicTest.Api
+      )
+    end
+
+    it "accepts :pretty for JSON output" do
+      assert {["swagger", "v1"], MaruSwaggerTest.BasicTest.Api, "v1", true, [] } == init(
+        at: "swagger/v1",
+        version: "v1",
+        pretty: true,
+        for: BasicTest.Api
+      )
+    end
+
+    it "accepts :prefix to prepend to URLs" do
+      assert {["swagger", "v1"], MaruSwaggerTest.BasicTest.Api, "v1", true, "/longish/prefix" } == init(
+        at: "swagger/v1",
+        version: "v1",
+        pretty: true,
+        prefix: "/longish/prefix",
+        for: BasicTest.Api
+      )
+    end
+  end
 end
